4298a3463336509da3461b2f456d734a7e843653
Aaron Warner <aaron.warner@concepts.co.nz>
2024-06-26 14:53:46 +1200

    update to use correct calls

--- lua/version.lua
+++
@@ -1,7 +0,0 @@
-local Version = "138"
----@alias VERSION "138"
----@return PATCH
-function GetVersion()
-    LOG('Supreme Commander: Forged Alliance Total Mayhem version ' .. Version)
-    return Version
-end
